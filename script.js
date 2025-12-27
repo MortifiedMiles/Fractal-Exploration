fdc74075cc0fa6faaba571e9985bd3765d38ba58
Miles <78830914+MortifiedMiles@users.noreply.github.com>
2025-12-26 20:58:00 -0700

    Beginnings of the project

---
+++ script.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three'
+
+
+// Scene
+const scene = new THREE.Scene()
+
+// Lighting
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
+
+// Rocket
+const sphere = new THREE.Sphere()
+scene.add(sphere)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+const tick = () =>
+{
+    // Time
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
