baf97676790ca904b50733f61b8b21ded668837e
Miles <78830914+MortifiedMiles@users.noreply.github.com>
2025-12-26 22:00:33 -0700

    Only thing left is the Fractal

--- script.js
+++ script.js
@@ -1,25 +1,87 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
+
+
 // Lighting
-const ambientLight = new THREE.AmbientLight()
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
-// Rocket
-const sphere = new THREE.Sphere()
-scene.add(sphere)
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.set(5, 5, 5)
+pointLight.lookAt(0, 0, 0)
+scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xffffff, 0.5)
+pointLight2.position.set(-5, -5, -5)
+pointLight2.lookAt(0, 0, 0)
+scene.add(pointLight2)
+
+
+// // Rocket
+// const rocket = new THREE.Mesh(
+//     new THREE.SphereGeometry(0.5, 32, 32),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(rocket)
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(3, 3, 3)
+camera.lookAt(0, 0, 0)
+scene.add(camera)
+
+// Controls
+const controls  = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.autoRotate = true
+controls.autoRotateSpeed = 0.5
+
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
 
 /**
  * Animate
@@ -27,7 +89,6 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 const tick = () =>
 {
-    // Time
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
